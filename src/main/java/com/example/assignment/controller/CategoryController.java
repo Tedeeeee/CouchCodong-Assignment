21bca89dd50333e4b731e141f26e8617440f9ea0
Tedeeeee <gksrmf359@gmail.com>
2023-08-18 15:26:42 +0900

    CategoryController Test With Postman

---
+++ src/main/java/com/example/assignment/controller/CategoryController.java
@@ -0,0 +1,16 @@
+package com.example.assignment.controller;
+
+import com.example.assignment.controller.dto.CategoryDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    @PostMapping("")
+    public CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO) {
+        return categoryDTO;
+    }
+}
